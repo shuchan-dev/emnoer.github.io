4df4acfb2a406887301c3c9cb0ca955cdddbe4ce
Ryhan Mandala <74956890+shuchan-dev@users.noreply.github.com>
2022-11-14 09:23:43 +0000

    bug fixed new

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
+// function sendMail() {
+//     var params = {
+//       name: document.getElementById("name").value,
+//       email: document.getElementById("email").value,
+//       message: document.getElementById("message").value,
+//     };
   
-    const serviceID = "service_zwkzyau";
-    const templateID = "template_e2e9tno";
+//     const serviceID = "service_zwkzyau";
+//     const templateID = "template_e2e9tno";
   
-      emailjs.sendForm(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
+//       emailjs.sendForm(serviceID, templateID, params)
+//       .then(res=>{
+//           document.getElementById("name").value = "";
+//           document.getElementById("email").value = "";
+//           document.getElementById("message").value = "";
+//           console.log(res);
+//           alert("Your message sent successfully!!")
   
-      })
-      .catch(err=>console.log(err));
+//       })
+//       .catch(err=>console.log(err));
   
-  }
+//   }
+
+
+  /* Sending Email from Contact Section */
+(function () {
+    emailjs.init("66A68JyoeUv1gZBP9");
+})();
+
+function sendmail() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+        var contactParams = {
+            from_name: name,
+            from_email: email,
+            message: message
+        };
+
+        emailjs.send('service_zwkzyau', 'template_e2e9tno', contactParams)
+        .then(res => {
+            console.log(res)
+            alert("Your message sent successfully!!")
+        })
+        .catch(err=>console.log(err));
+}
