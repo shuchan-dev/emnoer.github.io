d54f17e7d4f6c1309bb5230a68f3390d4dc3f806
shuchan-dev <robbyhafed17@gmail.com>
2022-11-24 13:50:27 +0700

    penambahan fitur dan bug fixed

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-// function sendMail() {
-//     var params = {
-//       name: document.getElementById("name").value,
-//       email: document.getElementById("email").value,
-//       message: document.getElementById("message").value,
-//     };
-  
-//     const serviceID = "service_zwkzyau";
-//     const templateID = "template_e2e9tno";
-  
-//       emailjs.sendForm(serviceID, templateID, params)
-//       .then(res=>{
-//           document.getElementById("name").value = "";
-//           document.getElementById("email").value = "";
-//           document.getElementById("message").value = "";
-//           console.log(res);
-//           alert("Your message sent successfully!!")
-  
-//       })
-//       .catch(err=>console.log(err));
-  
-//   }
-
-
-  /* Sending Email from Contact Section */
-
-
 function sendmail() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  let company = document.getElementById("company").value;
+  let telp = document.getElementById("telp").value;
 
-        var contactParams = {
-            name: name,
-            email: email,
-            message: message
-        };
+  var contactParams = {
+    name: name,
+    email: email,
+    message: message,
+    company: company,
+    telp: telp,
+  };
 
-        emailjs.send('service_zwkzyau', 'template_e2e9tno', contactParams)
-        .then(res => {
-            console.log(res)
-            alert("Your message sent successfully!!")
-        })
-        .catch(err=>console.log(err));
+  emailjs
+    .send("service_zwkzyau", "template_e2e9tno", contactParams)
+    .then((res) => {
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
 }
